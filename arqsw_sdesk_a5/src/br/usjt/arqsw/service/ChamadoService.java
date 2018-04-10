61ac4a9c017aec30a1f12d121f582bc0ee5bfbae
Lucas Nagaoka <lucasnagaoka@gmail.com>
2018-04-10 16:49:56 -0300

    Fechamento de chamado e adição de nova coluna na tabela Chamado

---
+++ arqsw_sdesk_a5/src/br/usjt/arqsw/service/ChamadoService.java
@@ -0,0 +1,55 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.usjt.arqsw.dao.ChamadoDAO;
+import br.usjt.arqsw.entity.Chamado;
+import br.usjt.arqsw.entity.Fila;
+
+/**
+ * 
+ * @author Lucas Nagaoka | RA: 81513916
+ *
+ */
+
+@Service
+public class ChamadoService {
+	ChamadoDAO dao;
+
+	@Autowired
+	public ChamadoService(ChamadoDAO dao) {
+		this.dao = dao;
+	}
+
+	public List<Chamado> listarChamados(Fila fila) throws IOException {
+		return dao.listarChamados(fila);
+	}
+
+	public Chamado criarChamado(Chamado chamado) throws IOException {
+		chamado.setStatus(Chamado.ABERTO);
+		chamado.setDataAbertura(new Date());
+		chamado.setDataFechamento(null);
+
+		Chamado chamadoSalvo = dao.criarChamado(chamado);
+
+		return chamadoSalvo;
+	}
+
+	public Chamado fecharChamado(int id) throws IOException {
+		Chamado chamado = dao.buscarPorId(id);
+		
+		chamado.setStatus(Chamado.FECHADO);
+		chamado.setDataFechamento(new Date());
+		
+		Chamado chamadoFechado = dao.fecharChamado(chamado);
+		
+		return chamadoFechado;
+		
+	}
+
+}
